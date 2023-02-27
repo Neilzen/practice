480feffbfa0cbbcc46901dd89b692d6762b73171
Neilzen Anthony De Torres <detorres_neilzen@yahoo.com>
2023-02-27 14:36:04 +0800

    Add Code Land Username Validation

---
+++ CodeTests/src/main/java/org/example/codelandusernamevalidation/CodelandUsernameValidation.java
@@ -0,0 +1,37 @@
+package org.example.codelandusernamevalidation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CodelandUsernameValidation {
+
+    public static String execute(String str) {
+        // Check length
+        if (str.length() < 4 || str.length() > 25) {
+            return "false";
+        }
+
+        // Check first character
+        char firstChar = str.charAt(0);
+        if (!Character.isLetter(firstChar)) {
+            return "false";
+        }
+
+        // Check last character
+        char lastChar = str.charAt(str.length() - 1);
+        if (lastChar == '_') {
+            return "false";
+        }
+
+        // Check for invalid characters
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9_]");
+        Matcher matcher = pattern.matcher(str);
+        if (matcher.find()) {
+            return "false";
+        }
+
+        return "true";
+    }
+
+
+}
---
+++ CodeTests/src/test/java/org/example/codelandusernamevalidation/CodelandUsernameValidationTest.java
@@ -0,0 +1,15 @@
+package org.example.codelandusernamevalidation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CodelandUsernameValidationTest {
+
+    @Test
+    void codelandUserNameValidation() {
+        assertEquals("false", CodelandUsernameValidation.execute("aa_"));
+        assertEquals("true", CodelandUsernameValidation.execute("u__hello_world123"));
+    }
+
+}
